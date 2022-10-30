b9b159eff3f6f5bdd031f33a58e6bc4140d7df8f
Daniel Robertson <danielrobertsonbusiness@gmail.com>
2022-10-30 14:03:37 +0100

    Completed exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //divisible by four
+    //divisible by 100 NO - unless divisble by 400 YES
+    if(year%4 == 0 && year%100!=0 || year%400==0){
+        return true;
+    }
+
+    return false;
 
 };
 
