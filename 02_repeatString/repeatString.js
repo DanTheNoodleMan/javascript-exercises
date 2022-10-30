b08574165955a9c97cda0bd6d197635d578cc871
Daniel Robertson <danielrobertsonbusiness@gmail.com>
2022-10-30 11:50:12 +0100

    Completed exercise with all .skip removed from the spec file

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    let repeated = '';
+    if(num < 0) return "ERROR";
+
+    for (let i = 0; i<num;i++){
+        repeated = repeated + word;
+    }
+    return repeated;
+    
 
 };
 
