7ccbc98d2c1620b7b718030541bc954560043921
Daniel Robertson <danielrobertsonbusiness@gmail.com>
2022-10-30 13:01:51 +0100

    Completed exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let charArray = new Array(word.length);
+    let result = '';
 
+    for(let i = 0; i<word.length; i++){
+        charArray[i] = word.charAt(i);
+    }
+
+    for (let j = charArray.length -1; j>=0; j--){
+        result = result + charArray[j];
+    }
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
