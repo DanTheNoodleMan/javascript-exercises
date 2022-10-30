8d45e02f1bf8eafadc89741e271b6809500767b3
Daniel Robertson <danielrobertsonbusiness@gmail.com>
2022-10-30 13:48:48 +0100

    Completed exercise using if else for the conditions where the first agument is bigger and vice verse

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if((typeof num1 != 'number' || typeof num2 != 'number') || (num1 < 0 || num2 < 0)){
+        return 'ERROR';
+    }
+
+    let sum = 0;
+    if(num1 < num2){
+        for (let i = num1; i<=num2; i++){
+            sum = sum + i;
+        }
+    } else if(num1 > num2){
+        for (let j = num1; j>=num2; j--){
+            sum = sum + j;
+        }
+    } else{
+        return sum = num1 + num2;
+    }
+
+    return sum;
+    
 
 };
 
