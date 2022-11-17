f555b4eb4e53d51f8855264c0b204e3d8b6ed6cd
Daniel Robertson <danielrobertsonbusiness@gmail.com>
2022-11-17 19:28:06 +0100

    Completed exercise and passed tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let rgx = /[\W_]/g;
+    let rgxStr = string.toLowerCase().replace(rgx, '');
+    let reverseStr = rgxStr.split('').reverse().join('');
+    return rgxStr == reverseStr;
 };
 
 // Do not edit below this line
